872658d1c6a1d34e7432263aa7bfd7f8eaf42966
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-11-04 10:49:09 +0530

    JS Basics 1 | Sprint 3 Day 1

---
+++ Sprint3-JS/Day-1/intro.js
@@ -0,0 +1,31 @@
+//Javascript-A programming language made in 10 days,to make interactive websites.
+// Analogy-HTML(skeleton),CSS(skin,hair,eyebrows),JS(Life)
+
+//Javascript-Java,
+//Code that are written in Js are known as scripts
+
+//JS-ES6=>ECHMAscript=>ECMA=>is standard for JS
+
+//Microsoft,DELL,APPLE set standard like ECMA
+//Also JS is an high level language
+
+//C lang:int A;=> static typing,where we are telling variable can only store of that data type.
+
+//JS:let A;Dynamically typed  language,where at given point variable can store any data type
+
+//Program in JS ,we use english terms so makes lang high level
+
+//Browser can only understand HTML,CSS,JS
+
+//Chrome browser has V8 engine which help to understand JS 
+
+//JS is a safe programming lang where it does not interact with files presnt in computer and also does not deal memory of device
+
+//Using this JS,we can manipulate the content on screen,which gives ability to control webpage
+
+//Js is actually known as Programming logic,because to run JS we need Just environment that supports JS.
+//Browser,NodeJs are such environments(capable of running JS)
+
+//NodeJS that helps your laptop understand compile and execute Javascript code
+
+//TypeScript:-static typed language and JS:-Dynamic Typing
