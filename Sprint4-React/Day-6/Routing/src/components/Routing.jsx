f48be64c68e21a209cdf8352df7c3188a733d52d
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-12-13 17:31:06 +0530

    React Basics 7 Sprint 4 Day 7:Dynamic Routing

--- Sprint4-React/Day-6/Routing/src/components/Routing.jsx
+++ Sprint4-React/Day-6/Routing/src/components/Routing.jsx
@@ -1,24 +1,73 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import {useState,useEffect,React} from 'react'
+import {Routes,Route,Link,useParams} from 'react-router-dom'
 
 function Routing() {
   return (
     <div>
         <nav>
           <ul>
-              <li>Home Page</li>
-              <li>About</li>
-              <li>Listing</li>
+              <li>
+                <Link to="/home">Home Page</Link>
+              </li>
+              <li>
+                <Link to="/about">About Page</Link>
+              </li>
+              <li>
+                <Link to="/Listing">Listing Page</Link>
+              </li>
           </ul>
         </nav>
         <Routes>
-            <Route path="/home" element={<Home></Home>}></Route>
-            <Route path="/about" element={<About></About>}></Route>
-            <Route path="/Listing" element={<Listing></Listing>}></Route>
-            <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Listing" element={<Listing />} />
+          <Route path="/users/:id" element={<Users isAdmin={true}></Users>}></Route>
+
+          {/* the below path '*' is a wildcard entry */}
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
+
     </div> 
   )
+  /*
+  1)Normal/Static Routing
+  2)Link Tag:-whenever we use it reload does not happen when we go from one page to other
+  3)Dynamic Routing /Template Routing
+  4)Nested Routing 
+  5)Custom Routing 
+  6)Redirect Routing
+  */ 
+
+  //Dynamic Routing
+  function Users(props) {
+    const params = useParams();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch(
+          `https://fakestoreapi.com/users/${params.id}`
+        );
+        const userData = await response.json();
+        console.log(userData)
+        setUser(userData);
+      };
+      fetchData();
+    },[params.id]);
+
+    return (
+    <div>
+      <h3>User Component</h3>
+      {user ? (
+        <p>
+          {user.name.firstname} {user.name.lastname}
+        </p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+  }
   function Home(){
     return (<h3>Home Page</h3>)
   }
